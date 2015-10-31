544fa13f858110d676f0a513e30a15c4e30f1e8b
KXStudio Guide <kxguideru@gmail.com>
2015-10-31 19:09:41 +0800

    Обновление

--- build.php
+++ build.php
@@ -1,6 +1,5 @@
 <?php
 
-define('OUTPUT_WRAP', 80);
 define('THUMB_WIDTH', 150);
 define('THUMB_HEIGHT', 150);
 
@@ -8,38 +7,39 @@ define('ROOT_PATH', __DIR__);
 define('OUTPUT_PATH', ROOT_PATH . '/www');
 define('SRC_PATH', ROOT_PATH . '/src');
 
-define('TEMPLATE', SRC_PATH . '/template.html');
-define('INDEX', OUTPUT_PATH . '/index.html');
-
 define('MSG_OPEN_IMAGE', 'Показать изображение');
 
-function cleanup($output_path, $index) {
-    $images = $output_path . '/images';
-    $thumbs = $images . '/thumbs';
+/**
+ * Cleanup output
+ *
+ * @param $output_path
+ */
+function cleanup($output_path)
+{
+    $paths = [
+        '',
+        '/images',
+        '/images/thumbs',
+        '/css',
+        '/presets',
+    ];
 
-    if (!file_exists($images)) {
-        mkdir($images);
-    }
-
-    if (!file_exists($thumbs)) {
-        mkdir($thumbs);
-    }
-
-    cleanup_dir($images);
-    cleanup_dir($thumbs);
-
-    if (file_exists($index)) {
-        unlink($index);
-    }
-
-    $css = $output_path . '/css/styles.css';
-
-    if (file_exists($css)) {
-        unlink($css);
+    foreach ($paths as $path) {
+        cleanup_dir($output_path . $path);
     }
 }
 
-function cleanup_dir($path) {
+/**
+ * Cleanup directory
+ *
+ * @param $path
+ */
+function cleanup_dir($path)
+{
+    if (!file_exists($path)) {
+        mkdir($path, 0777, true);
+    }
+
     $files = glob("$path/*");
 
     foreach ($files as $file) {
@@ -49,24 +49,40 @@ function cleanup_dir($path) {
     }
 }
 
-function copy_image($source_path, $dest_path, $filename) {
+/**
+ * Copy image and create thumbnail
+ *
+ * @param $source_path
+ * @param $dest_path
+ * @param $filename
+ */
+function copy_image($source_path, $dest_path, $filename)
+{
     $source = $source_path . '/' . $filename;
     $dest = $dest_path . '/' . $filename;
     $thumb = $dest_path . '/thumbs/thumb-' . $filename;
 
     if (!file_exists($dest) || !file_exists($thumb) ||
-            filemtime($dest) < filemtime($source) ||
-            filemtime($thumb) < filemtime($source)) {
+        filemtime($dest) < filemtime($source) ||
+        filemtime($thumb) < filemtime($source)
+    ) {
         copy($source, $dest);
         $imagick = new \Imagick(realpath($dest));
-        $imagick->setbackgroundcolor('rgb(64, 64, 64)');
+        $imagick->setBackgroundColor('rgb(64, 64, 64)');
         $imagick->thumbnailImage(THUMB_WIDTH, THUMB_HEIGHT, true);
-        $blob = $imagick->getimageblob();
+        $blob = $imagick->getImageBlob();
         file_put_contents($thumb, $blob);
     }
 }
 
-function image_callback($matches) {
+/**
+ * Callback for image search pattern, create thumbnail and lightbox
+ *
+ * @param $matches
+ * @return string
+ */
+function image_callback($matches)
+{
     $filename = $matches[1];
     $image_name = $matches[2];
     copy_image(SRC_PATH . '/images', OUTPUT_PATH . '/images', $filename);
@@ -79,52 +95,94 @@ function image_callback($matches) {
     return $lightbox;
 }
 
-function template_callback($matches) {
+/**
+ * Callback for template pattern, insert file
+ *
+ * @param $matches
+ * @return mixed
+ */
+function template_callback($matches)
+{
     $filename = $matches[1];
     $html = file_get_contents('src/' . $filename);
     $image_pattern = '/\<img src=\"images\/(([^\.]+)\.png)\"[^\>]+\>/';
     return preg_replace_callback($image_pattern, "image_callback", $html);
 }
 
-function simple_copy($path_src, $path_out, $relative) {
+/**
+ * Copy file
+ *
+ * @param $path_src
+ * @param $path_out
+ * @param $relative
+ */
+function copy_file($path_src, $path_out, $relative)
+{
     $out = $path_out . '/' . $relative;
     $src = $path_src . '/' . $relative;
-    
+
     if (!file_exists($out) || filemtime($out) < filemtime($src)) {
         copy($src, $out);
     }
 }
 
-function build($template_file, $output_file) {
-    $path_out = dirname($output_file);
-    $path_src = dirname($template_file);
-    $out_css_path = $path_out . '/css';
-        
-    if (!file_exists($out_css_path)) {
-        mkdir($out_css_path);
+/**
+ * Copy all found files
+ *
+ * @param $path_src
+ * @param $path_out
+ */
+function copy_files($path_src, $path_out)
+{
+    $files = glob("$path_src/*");
+
+    foreach ($files as $file) {
+        if (is_file($file)) {
+            copy_file($path_src, $path_out, basename($file));
+        }
     }
-    
-    simple_copy($path_src, $path_out, 'css/style.css');
-    simple_copy($path_src, $path_out, 'images/background.png');
+}
+
+/**
+ * Build output
+ *
+ * @param $path_src
+ * @param $path_out
+ */
+function build($path_src, $path_out)
+{
+    $template_file = $path_src . '/template.html';
+    $index_file = $path_out . '/index.html';
+
+    copy_file($path_src, $path_out, 'css/style.css');
+    copy_file($path_src, $path_out, 'images/background.png');
+    copy_files($path_src . '/presets', $path_out . '/presets');
 
     $template = file_get_contents($template_file);
     $html = preg_replace_callback('/\{\{([^}]+)\}\}/', "template_callback", $template);
-    file_put_contents($output_file, $html);
+    file_put_contents($index_file, $html);
 }
 
-function main($argc, $argv) {
+/**
+ * Entry point
+ *
+ * @param $argc
+ * @param $argv
+ */
+function main($argc, $argv)
+{
     if ($argc > 1) {
         $job = $argv[1];
 
         switch ($job) {
             case "clean":
                 echo "Cleaning up...\n";
-                cleanup(OUTPUT_PATH, INDEX);
+                cleanup(OUTPUT_PATH);
                 return;
 
             case "build":
                 echo "Building...\n";
-                build(TEMPLATE, INDEX);
+                build(SRC_PATH, OUTPUT_PATH);
                 return;
 
             default:
